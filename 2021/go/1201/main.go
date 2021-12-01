f7881ac719491ee1148933c21bcda551bd9a311e
Hector Rios <that.hector@gmail.com>
2021-12-01 14:21:14 -0800

    Finish day1.ex2, plus minor clean up

--- 2021/go/1201/main.go
+++ 2021/go/1201/main.go
@@ -9,30 +9,48 @@ import (
 
 func CountRollingIncrements(numberSequence []int) int {
 	count := 0
-	lastNumber := numberSequence[0]
 
-	for _, number := range numberSequence {
-		if number > lastNumber {
+	for i := 1; i < len(numberSequence); i++ {
+		if numberSequence[i] > numberSequence[i-1] {
 			count += 1
 		}
-
-		lastNumber = number
 	}
 
 	return count
 }
 
-func main() {
+func CountRollingIncrementsOfThree(numberSequence []int) int {
+	count := 0
+	lastSum := 0
+
+	for i := 2; i < len(numberSequence); i++ {
+		sum := numberSequence[i-2] + numberSequence[i-1] + numberSequence[i]
+
+		if i > 2 && sum > lastSum {
+			count += 1
+		}
+
+		lastSum = sum
+	}
+
+	return count
+}
+
+func loadInput() ([]int, error) {
+	var (
+		ints []int
+		err  error
+	)
 	data, err := os.ReadFile("input.txt")
 
 	if err != nil {
 		fmt.Println("Unable to read input.txt")
-		panic(err)
+		return ints, err
 	}
 
 	dataAsString := string(data)
 	inputAsStrings := strings.Split(dataAsString, "\n")
-	ints := make([]int, len(inputAsStrings)-1)
+	ints = make([]int, len(inputAsStrings)-1)
 
 	for idx, d := range inputAsStrings {
 		cleanString := strings.TrimSpace(d)
@@ -42,14 +60,26 @@ func main() {
 
 			if err != nil {
 				fmt.Printf("something went wrong reading input at index %d\n", idx)
-				panic(err)
+				return ints, err
 			}
 
 			ints[idx] = i
 		}
 	}
 
-	counts := CountRollingIncrements(ints)
+	return ints, err
+}
 
-	fmt.Printf("Found a total of %d instances where a depth measurement increases", counts)
+func main() {
+	ints, err := loadInput()
+
+	if err != nil {
+		panic(err)
+	}
+
+	counts := CountRollingIncrements(ints)
+	fmt.Printf("Found a total of %d instances where a depth measurement increases\n", counts)
+
+	counts = CountRollingIncrementsOfThree(ints)
+	fmt.Printf("Found a total of %d sequences of three numbers where a depth measurement increases\n", counts)
 }
--- 2021/go/1201/main_test.go
+++ 2021/go/1201/main_test.go
@@ -16,3 +16,16 @@ func TestCountRollingIncrements(t *testing.T) {
 		}
 	})
 }
+
+func TestCountRollingIncrementsOfThree(t *testing.T) {
+	t.Run("It returns the number of rolling increments", func(t *testing.T) {
+		input := []int{607, 618, 618, 617, 647, 716, 769, 792}
+		want := 5
+
+		got := CountRollingIncrementsOfThree(input)
+
+		if got != want {
+			t.Errorf("got %d, want %d", got, want)
+		}
+	})
+}
